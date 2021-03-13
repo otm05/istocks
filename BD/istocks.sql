3c12a5cdb9b57981be48fe17595567b33801750a
ouiam05 <ouiamtisbah2000@gmail.com>
2021-03-13 15:22:16 +0100

    final

--- BD/istocks.sql
+++ BD/istocks.sql
@@ -182,65 +182,6 @@ insert into Collaborateurs values('C3','shop','xxxxx12','r','Dd','FD','C','C','D
 
 --G2
 
---Table Fournisseurs
-create table Fournisseurs
-(
- codeFour varchar(20),
-  codeEntrFk varchar(20),
-  nomFour varchar(80),
-  raisonSocialFour varchar(255),
-  description varchar(255),
-  secteur varchar(20),
-  categorie int,
-  adresse varchar(255),
-  ville varchar(80),
-  codePostale varchar(10),
-  pays varchar(80),
-  email varchar(150),
-  fixe varchar(15),
-  fax varchar(15),
-  siteWeb varchar(150),
-  fullNameContact varchar(100),
-  gsmContact varchar(15),
-  logoFour varchar(50),
-  constraint pk_Four primary key(codeFour,codeEntrFk)
-);
-alter table Fournisseurs add foreign key (codeEntrFk) references Entreprises(codeEntr) on delete cascade;
-
---Table Categories
-create table Categories(
-     codeCat varchar(20) ,
-     codeEntrFk varchar(20),
-     codeFmlFk varchar(20) ,
-     nomCat varchar(80),
-     description varchar(255),
-     constraint pk_ctg primary key(codeCat,codeEntrFk)
-);
-alter table Categories add foreign key (codeEntrFk) references Entreprises(codeEntr) on delete cascade;
-alter table Categories add foreign key (codeFmlFk) references Familles(codeFml) on delete cascade;
-
---Table Unites
-create table Unites(
-    codeUnit varchar(20),
-    codeEntrFk varchar(20) null,
-    nomUnit varchar(50),
-    typeUnit varchar(50),
-    constraint pk_Unt primary key(codeUnit,codeEntrFk)
-);
-alter table Unites add foreign key (codeEntrFk) references Entreprises(codeEntr) on delete cascade;
-
---Table Casier
-create table Casiers(
-    numCasier varchar(10) ,
-    codeEntrFk varchar(20) null,
-    codeRangFk varchar(20) null,
-    forme varchar(225),
-    description varchar(255),
-    constraint pk_Cs primary key(numCasier,codeEntrFk)
-);
-alter table Casiers add foreign key (codeEntrFk) references Entreprises(codeEntr) on delete cascade;
-alter table Casiers add foreign key (codeRangFk) references Rangs(codeRang) on delete cascade;
-
 create table Marques(
     codeMrq varchar(20),
     codeEntrFk varchar(20) null,
@@ -283,43 +224,4 @@ create table Rangs(
 );
 alter table Rangs add foreign key (codeEntrFk) references Entreprises(codeEntr) on delete cascade;
 alter table Rangs add foreign key (codeMagFk) references Magasins(codeMag) on delete cascade;
-
---Les Inserts:
-
-insert into Casiers values('numCas1','E1','Rang1','forme1','descr1');
-insert into Casiers values('numCas2','E1','Rang1','forme2','descr2');
-
-insert into Rangs values('Rang1','Mg1','E1','desc1');
-
-insert into Magasins values('Mg1','E1','nomMG1','Adresse1','ville1','pays2','Desc1');
-
-insert into Unites values('Unit1','E1','nomUnit1','typeUnit1');
-insert into Unites values('Unit2','E1','nomUnit2','typeUnit3');
-insert into Unites values('Unit3','E1','nomUnit3','typeUnit4');
-
-
-insert into Marques values('1','E1','marq1','L','des1','pays1','logo1');
-insert into Marques values('2','E1','marq2','C','des2','pays2','logo2');
-insert into Marques values('3','E1','marq3','LC','des3','pays3','logo3');
-insert into Marques values('4','E1','marq4','L','des4','pays4','logo4');
-insert into Marques values('5','E1','marq5','N','des5','pays5','logo5');
-
-insert into Fournisseurs values('F1','E1','nomF1','RaisF1','descr1','secteur1',1,'adresse1','ville1','codeP1','pays1','email1','0987654321','05432466876','siteW1','fullname1','gsm1','logo1');
-insert into Fournisseurs values('F2','E1','nomF2','RaisF2','descr2','secteur2',2,'adresse2','ville2','codeP2','pays2','email2','0987654321','05432466876','siteW2','fullname2','gsm2','logo2');
-insert into Fournisseurs values('F3','E1','nomF3','RaisF3','descr3','secteur3',3,'adresse3','ville3','codeP3','pays3','email3','0987654321','05432466876','siteW3','fullname3','gsm3','logo3');
-insert into Fournisseurs values('F4','E1','nomF4','RaisF4','descr4','secteur4',4,'adresse4','ville4','codeP4','pays4','email4','0987654321','05432466876','siteW4','fullname4','gsm4','logo4');
-insert into Fournisseurs values('F5','E1','nomF5','RaisF5','descr5','secteur5',5,'adresse5','ville5','codeP5','pays5','email5','0987654321','05432466876','siteW5','fullname5','gsm5','logo5');
-
-insert into Categories values('cat1','E1','fm1','nomct1','desc1');
-insert into Categories values('cat2','E1','fm2','nomct2','desc2');
-insert into Categories values('cat3','E1','fm3','nomct3','desc3');
-insert into Categories values('cat4','E1','fm4','nomct4','desc4');
-insert into Categories values('cat5','E1','fm5','nomct5','desc5');
-
-insert into Familles values('fm1','E1','nomfm1','desc1');
-insert into Familles values('fm2','E1','nomfm2','desc2');
-insert into Familles values('fm3','E1','nomfm3','desc3');
-insert into Familles values('fm4','E1','nomfm4','desc4');
-insert into Familles values('fm5','E1','nomfm5','desc5');
-
 
